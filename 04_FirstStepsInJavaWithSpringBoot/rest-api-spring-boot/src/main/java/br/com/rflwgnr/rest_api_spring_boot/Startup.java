1113476b72f747bfd9d9173fa3d0a7da647a170e
RafaWagnerTI <rafaelwagner2000@gmail.com>
2025-07-06 23:36:27 -0300

    Primeiros passos em spring boot

---
+++ 04_FirstStepsInJavaWithSpringBoot/rest-api-spring-boot/src/main/java/br/com/rflwgnr/rest_api_spring_boot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.rflwgnr.rest_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ 04_FirstStepsInJavaWithSpringBoot/rest-api-spring-boot/src/main/java/br/com/rflwgnr/rest_api_spring_boot/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.rflwgnr.rest_api_spring_boot.model;
+
+public record Greeting(long id, String content) {
+
+}
