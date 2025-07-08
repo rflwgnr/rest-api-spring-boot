edf5b715cf4a095b50e3854d84757e4e1c962a6a
RafaWagnerTI <rafaelwagner2000@gmail.com>
2025-07-07 23:08:47 -0300

    desafio proposto em aula

--- 04_FirstStepsInJavaWithSpringBoot/rest-api-spring-boot/src/main/java/br/com/rflwgnr/rest_api_spring_boot/controllers/MathController.java
+++ 04_FirstStepsInJavaWithSpringBoot/rest-api-spring-boot/src/main/java/br/com/rflwgnr/rest_api_spring_boot/controllers/MathController.java
@@ -1,10 +1,8 @@
 package br.com.rflwgnr.rest_api_spring_boot.controllers;
 
-
 import br.com.rflwgnr.rest_api_spring_boot.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +14,41 @@ public class MathController {
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please set a numeric value!");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/subtract/{numberOne}/{numberTwo}")
+    public Double subtract(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/multiply/{numberOne}/{numberTwo}")
+    public Double multiply(@PathVariable("numberOne") String numberOne,
+                           @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/divide/{numberOne}/{numberTwo}")
+    public Double divide(@PathVariable("numberOne") String numberOne,
+                           @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/average/{numberOne}/{numberTwo}/{numberThree}")
+    public Double average(@PathVariable("numberOne") String numberOne,
+                          @PathVariable("numberTwo") String numberTwo,
+                          @PathVariable("numberThree") String numberThree) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo) || !isNumeric(numberThree)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo) + convertToDouble(numberThree)) / 3;
+    }
+
+    @RequestMapping("/sqrt/{number}")
+    public Double sqrt(@PathVariable("number") String number) throws Exception {
+        if(!isNumeric(number)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return Math.sqrt(convertToDouble(number));
     }
 
     private Double convertToDouble(String strNumber) throws UnsupportedMathOperationException {
